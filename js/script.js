e58727b0451e16fc50ea573391f569e9e87b7bef
Maksim <117210740+Pelsid@users.noreply.github.com>
2023-04-06 12:37:04 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var burger = document.querySelector("#burger-menu");
+var menu = document.querySelector("#menuLinks");
+
+function showMenu () {
+	burger.classList.toggle("show")
+	menu.classList.toggle("show")
+}
+
+console.log('link to the layout' + 'https://www.figma.com/file/8bnLumYosiiSPOPxXE4cob/John?node-id=0-1&t=KERdYJGhxW5xiRvs-0');
